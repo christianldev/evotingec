cc4e6091788b0147ea82fff4e73d9e8259fe25b9
christianldev <christlopbsc12@gmail.com>
2023-08-12 00:11:59 -0500

    Implementacion de validacion de cedula

---
+++ Client/src/services/AuthService.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API = import.meta.env.VITE_API;
+
+export const authenticateUser = (data) => {
+	const {nationalId} = data;
+
+	return new Promise((resolve, reject) => {
+		// get user by nationalId and send in body to backend
+		axios
+			.get(API + '/auth', {
+				headers: {nationalId: nationalId},
+			})
+			.then((r) => {
+				console.log(r);
+				resolve(r);
+			})
+			.catch((err) => {
+				console.log(err);
+				reject(err);
+			});
+	});
+};
